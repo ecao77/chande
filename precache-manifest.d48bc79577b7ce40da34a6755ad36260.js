5fc1f9791c3bb804b2646585c333fd14f1b5e220
Enbao Cao <ecao25@sjs.org>
2024-03-29 09:13:00 -0500

    Updates

---
+++ precache-manifest.d48bc79577b7ce40da34a6755ad36260.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0598e6e2706befa1d3c",
+    "url": "/chande/static/css/main.06cf9be0.chunk.css"
+  },
+  {
+    "revision": "c0598e6e2706befa1d3c",
+    "url": "/chande/static/js/main.102727da.chunk.js"
+  },
+  {
+    "revision": "eafff7ee506984aa33e5",
+    "url": "/chande/static/js/runtime~main.8b4b124b.js"
+  },
+  {
+    "revision": "c6753f249d3609eb5988",
+    "url": "/chande/static/js/2.0792de74.chunk.js"
+  },
+  {
+    "revision": "85b67d58420062fbecec",
+    "url": "/chande/static/js/3.eff08ea3.chunk.js"
+  },
+  {
+    "revision": "719d9977ff93d32f531a473bcbb7cacc",
+    "url": "/chande/static/media/logo.719d9977.svg"
+  },
+  {
+    "revision": "cb22bf98ca1952a6ade531999f177e2e",
+    "url": "/chande/index.html"
+  }
+];
